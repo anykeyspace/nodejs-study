5291fecda5f451b8df0d06e965b1a2217280eec6
Yuri Semikov <daario2012@gmail.com>
2019-02-26 18:35:12 +0300

    Node.JS screencast lessons 01-06

---
+++ modules/start/user/index.js
@@ -0,0 +1,16 @@
+var db = require('db');
+var log = require('logger')(module);
+
+function User(name) {
+    this.name = name;
+}
+
+User.prototype.hello = function (who) {
+    log(db.getPhrase("Hello") + ", " + who.name);
+};
+
+// console.log("user.js is required!");
+
+module.exports = User;
+
+// console.log(module);

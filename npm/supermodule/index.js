f9f632fa153898aa88673a208f490faddbb7f176
Yuri Semikov <daario2012@gmail.com>
2019-02-27 18:56:53 +0300

    07-10. NPM intro

---
+++ npm/supermodule/index.js
@@ -0,0 +1,28 @@
+// Основные команды NPM
+
+// npm init -> package.json
+
+// npm adduser
+// npm addu
+
+// npm publish
+// npm pu
+
+// npm search {keywords}
+// npm s {keywords}
+
+// npm install module
+// npm i module
+
+// npm update module
+// npm up module
+
+// npm remove module
+// npm r module
+
+// npm help command
+
+
+module.exports = function() {
+    console.log("Some module.");
+};

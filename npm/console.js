b23928268821ec02c3e4e69adc3ce34e6dd98060
Yuri Semikov <daario2012@gmail.com>
2019-02-28 19:02:52 +0300

    11. console

---
+++ npm/console.js
@@ -0,0 +1,11 @@
+// Модуль console
+// Использует util.format и util.inspect
+// console - глобальная переменная
+
+console.log("log"); // = info (out)
+
+console.error("error"); // = warn (err)
+
+// NO console.debug
+
+console.trace(); // Выводит текущий стек-трейс в поток ошибок (err)

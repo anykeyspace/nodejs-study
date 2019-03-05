d1c45a95f6a6ffb8e1c29a4958b5076f02408e68
Anykey Space <20nomad12@mail.ru>
2019-03-05 23:55:42 +0300

    13. EventEmitter

---
+++ top/ee.js
@@ -0,0 +1,27 @@
+// аргументы передаются по цепочке
+// обработчики срабатывают в порядке назначения
+
+var EventEmitter = require('events').EventEmitter;
+
+var server = new EventEmitter;
+
+// подписка на событие
+server.on('request', function (request) {
+    request.approved = true;
+});
+
+server.on('request', function (request) {
+    console.log(request);
+});
+
+server.on('error', function (err) {
+    // ...
+});
+
+// emit генерирует события
+server.emit('request', {from: "Клиент"});
+
+server.emit('request', {from: "Еще клиент"});
+
+server.emit('error'); // throw TypeError
+server.emit('error', new Error("Серверная ошибка"));
